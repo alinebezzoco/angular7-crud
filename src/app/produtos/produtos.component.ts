c0436379d6dfa1f0a6b016f826d9212ccb2fe7b5
Aline Bezzoco <contato.alinebezzoco@gmail.com>
2019-05-14 00:06:14 -0300

    Update files

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,35 @@
+import { ApiService } from './../../service/api.service';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/model/produto';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.scss']
+})
+export class ProdutosComponent implements OnInit {
+
+  displayedColumns: string[] = [ 'nome', 'desc', 'preco', 'acao'];
+  dataSource: Produto[];
+
+  isLoadingResults = true;
+
+  constructor(private _api: ApiService) { }
+
+  ngOnInit() {
+    this.displayAllProducts();
+  }
+
+  displayAllProducts() { 
+    this._api.getProdutos()
+    .subscribe(res => {
+      this.dataSource = res;
+      console.log(this.dataSource);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
+  }
+
+}
